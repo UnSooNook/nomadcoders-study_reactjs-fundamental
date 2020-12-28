399b9815dab741beb279916c81efa90dfc3ee00c
SOONWOOK PARK <soonwook34@gmail.com>
2020-12-28 15:44:04 +0900

    #2.3 map Recap

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -1,25 +1,21 @@
 import React from "react";
 
 function App() {
-  return (
-    <div>
-      {foodILike.map(dish => <Food name={dish.name} picture={dish.image}/>)}
-    </div>
-  );
+  return <div>{foodILike.map(dish => <Food key={dish.id} name={dish.name} picture={dish.image}/>)}</div>
 }
 
 const foodILike = [
-  { name: "kimchi", image: "https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=700%2C636" },
-  { name: "ramen", image: "https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=700%2C636" },
-  { name: "삼겹살", image: "https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=700%2C636" },
-  { name: "쭈꾸미", image: "https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=700%2C636" }
+  { id:1, name: "kimchi", image: "https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=700%2C636" },
+  { id:2, name: "ramen", image: "https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=700%2C636" },
+  { id:3, name: "삼겹살", image: "https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=700%2C636" },
+  { id:4, name: "쭈꾸미", image: "https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=700%2C636" }
 ]
 
 function Food({ name, picture }) {
   return (
     <div>
       <h3>I like {name}</h3>
-      <img src={picture} />
+      <img src={picture} alt={name}/>
     </div>
   );
 }
